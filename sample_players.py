b9479c16c0701400b1f6bd4f6e1abc98fb3d6353
swaption2009 <swaption2009@gmail.com>
2017-03-26 09:19:41 -0700

    resolve conflict

--- sample_players.py
+++ sample_players.py
@@ -134,7 +134,7 @@ class GreedyPlayer():
     equivalent to a minimax search agent with a search depth of one.
     """
 
-    def __init__(self, score_fn=open_move_score):
+    def __init__(self, score_fn=improved_score):
         self.score = score_fn
 
     def get_move(self, game, legal_moves, time_left):
