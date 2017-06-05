b8937c44088cf3e6f16396ff59d16030e4a712da
Chris Gearhart <cgearhart@gmail.com>
2017-06-05 09:58:37 -0700

    FIX remove unnecessary comparison in tallying forfeits in tournament.py

--- tournament.py
+++ tournament.py
@@ -65,7 +65,7 @@ def play_round(cpu_agent, test_agents, win_counts, num_matches):
 
         if termination == "timeout":
             timeout_count += 1
-        elif winner not in test_agents and termination == "forfeit":
+        elif termination == "forfeit":
             forfeit_count += 1
 
     return timeout_count, forfeit_count
